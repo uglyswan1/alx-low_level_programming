ba4dc40944aef5bbf0e8499a60ed7c20300b77c0
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-20 18:51:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Printig a sentence using puts() method'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{   
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
