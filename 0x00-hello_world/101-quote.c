8386298e0a9dc5f5032381fab0859a6b2dc92abf
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-20 19:54:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch = 'and that piece of art is useful\" - Dora Korpar, 2015-10-19';
+putchar(ch);
+return 0;
+}
