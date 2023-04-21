4d7943795a4b9467075a068b96e4578acc566696
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-20 18:37:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,7 @@
-puts('Programming is like building a multilingual puzzle');
+#include<stdio.h>
+
+int main()
+{   
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
+}
