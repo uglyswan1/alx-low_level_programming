421e0fbe0f5a61ff471b90df4cdad06e6c91f2a4
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-21 18:11:08 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,10 @@ int main(void)
 int i, j;
 for (i = 0; i < 10; i++)
 {
-putchar(78 + i + ', ');
+putchar(78 + i);
 for (j = i+1; j < 10; j++)
 {
-putchar(48 + j + ', ');
+putchar(48 + j);
 }
 }
 putchar('\n');
