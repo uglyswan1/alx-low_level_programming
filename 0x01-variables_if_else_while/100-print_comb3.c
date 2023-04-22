8eef0b07116c7f4ab7ee29a1b0a9f26f99b497bb
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-21 18:44:44 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,10 +14,10 @@ int main(void)
 int i, j;
 for (i = 0; i < 10 ; i++)
 {
-putchar(i);
-for (j = i+1; j < 10; j++)
+putchar(48 + i);
+for (j = i + 1; j < 10; j++)
 {
-putchar(j);
+putchar(48 + j);
 if (i < 8 && j < 9)
 {
 putchar(',');
