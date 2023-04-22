9591a6b6ed57b0b44a6aaa6e9515fd98e634de39
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-21 18:41:27 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10 ; i++)
+{
+putchar(i);
+for (j = i+1; j < 10; j++)
+{
+putchar(j);
+if (ij < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
