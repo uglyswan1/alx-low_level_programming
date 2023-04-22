eb6fe310038aaacab2b53d1f1251fb9b5d593b6b
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-21 20:15:52 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,11 +16,11 @@ for (i = 0; i < 10 ; i++)
 {
 for (j = 0; j < 10; j++)
 {
+for (k = 0; k < 10; k++)
+{
 putchar(48 + i);
 putchar(48 + j);
 putchar(' ');
-for (k = 0; k < 10; k++)
-{
 putchar(48 + j);
 putchar(48 + k);
 }
