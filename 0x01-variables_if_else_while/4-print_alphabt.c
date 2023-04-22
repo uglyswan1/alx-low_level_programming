5b729426ea248fcf5aa4a705a2f361299b3400e1
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-21 17:10:36 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if(ch !== 'e' || ch !== 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
