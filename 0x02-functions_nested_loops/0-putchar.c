f786ecfa53b2b79689476c5c6d59fcaf7400c93c
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-25 16:19:26 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+char ch = '_putchar';
+putchar(ch);
+putchar('\n');
+return (0);
+}
