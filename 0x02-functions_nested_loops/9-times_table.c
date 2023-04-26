6b026f7ae298e689b763217aef22917ddceb65cc
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-26 12:06:01 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,15 +15,12 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 k = i * j;
-if (j != 9)
-{
-if (k <= 9)
+if (k < 10 && j != 0)
 {
 _putchar(k + '0');
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-}
 }
 else if (k > 9)
 {
@@ -33,6 +30,6 @@ _putchar((k / 10) + '0');
 _putchar((k & 10) + '0');
 }
 }
-}
 _putchar('\n');
+}
 }
