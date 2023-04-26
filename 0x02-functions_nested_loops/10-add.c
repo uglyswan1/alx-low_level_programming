1c4a13d97a6c5660bbf88ecff3f7c8771d773861
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-26 15:51:07 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,6 +5,9 @@
 /* more headers goes there */
 /**
  * add -  adds two integers
+ * @a: integer number 1
+ * @b: integer number 2
+ * Return: the result of the addition of a and b
 */
 /* betty style doc for function main goes there */
 int add(int a, int b)
