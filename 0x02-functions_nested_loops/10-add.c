5c7d47365fe6a142960baaf31e85546f286f65de
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-26 15:47:04 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+/**
+ * add -  adds two integers
+*/
+/* betty style doc for function main goes there */
+int add(int a, int b)
+{
+return (a + b);
+}
