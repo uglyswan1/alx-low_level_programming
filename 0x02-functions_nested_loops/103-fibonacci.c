a476904acc2a94714c18d86650bd000cc6babf8c
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-26 18:44:23 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i;
+unsigned long sum = 0;
+unsigned long a = 1;
+unsigned long b = 2;
+unsigned long c = 0;
+sum = a + b;
+while (sum < 4000000)
+{
+a = b;
+b = sum;
+sum = a + b;
+if ((sum % 2) == 0)
+{
+c = c + sum;
+}
+}
+printf("%lu\n", c);
+return (0);
+}
