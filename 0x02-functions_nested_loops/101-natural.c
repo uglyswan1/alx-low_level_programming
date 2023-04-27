125729038804ad9753ba6e9f83edee7283d0952e
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-26 18:00:13 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,11 +14,11 @@ int i;
 int sum = 0;
 for (i = 0; i < 1024; i++)
 {
-if (((i % 3) == 0) && ((i % 5) == 0))
+if (((i % 3) == 0) || ((i % 5) == 0))
 {
 sum = sum + i;
 }
 }
 printf("%d\n", sum);
-return(0);
+return (0);
 }
