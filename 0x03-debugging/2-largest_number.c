e997b63b7b2c4fe56f929b289cbf25831e5695b0
uglyswan1 <knza.hrsn.14@gmail.com>
2023-04-28 16:41:11 -0700

    3-print_remaining_days

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,21 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a > b && a > c)
     {
         largest = a;
     }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (c > a && c > b)
+    else if (a > b && c > a)
     {
         largest = c;
     }
-
+    else if (b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
     return (largest);
 }
