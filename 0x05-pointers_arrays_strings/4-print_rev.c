44172114e9a6e13f248c62af4bc352f150a939c4
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-02 17:49:40 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line
- * @s: string to be printed 
+ * @s: string to be printed
  */
 void print_rev(char *s)
 {
@@ -19,4 +19,5 @@ _putchar(*s);
 s--;
 }
 }
+_putchar('\n');
 }
