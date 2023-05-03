37cc655e789295675b1c8c1f3a0ef2956282b549
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-03 11:20:37 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2  -  prints every other character of a string
+ * @str: string to print 
+ */
+void puts2(char *str)
+{
+int length = 0;
+int j = 0;
+while (str[j] != '\0')
+{
+length++;
+j++;
+}
+for (j = 0; j < length; j = j + 2)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
