c05da2fac0195de741b463916ff6f890b975c857
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-03 11:38:03 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string
+ * @str: string to print
+ **/
+void puts_half(char *str)
+{
+int i = 0;
+int length = 0;
+while (str[i] != '\0')
+{
+length++;
+i++;
+}
+if ((length % 2) == 1)
+{
+length = length - 1;
+}
+for (i = length / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
