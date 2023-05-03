df64169b5c36a832d7bba68a56fe7b5c0e07391f
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-03 16:43:07 -0700

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ */
+int _atoi(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= '0' && s[i] <= '9')
+{
+if (s[i-1] == '+')
+{
+_putchar('+');
+}
+else if (s[i-1] == '-')
+{
+_putchar('-');
+}
+_putchar(s[i]);
+}
+else
+{
+_putchar('0');
+}
+}
+}
