4b6f31252b268b962e4f52a81d02e8e936b9d12c
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-03 12:08:51 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,6 +6,7 @@
  **/
 void puts_half(char *str)
 {
+int n;
 int i = 0;
 int length = 0;
 while (str[i] != '\0')
@@ -15,9 +16,13 @@ i++;
 }
 if ((length % 2) == 1)
 {
-length = length - 1;
+n = (length - 1) / 2;
 }
-for (i = length / 2; str[i] != '\0'; i++)
+else
+{
+n = length / 2;
+}
+for (i = n; str[i] != '\0'; i++)
 {
 _putchar(str[i]);
 }
