e47136aa2dc68c3d5fdea2497ce18e9417bceab9
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-03 18:14:23 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,28 +7,33 @@
  */
 int _atoi(char *s)
 {
-int i, d;
-int n = 0;
-for (i = 0; s[i] != '\0'; i++)
+int i, d, n, len, f, digit;
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
 {
+if (s[i] == '-')
+++d;
 if (s[i] >= '0' && s[i] <= '9')
 {
-if (s[i - 1] == '+')
-{
-_putchar('+');
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
 }
-else if (s[i - 1] == '-')
-{
-d = s[i] - '0';
-n = n * 10 - d;
+i++;
 }
-d = s[i] - '0';
-n = n * 10 + d;
-}
-else
-{
+if (f == 0)
 return (0);
-}
-}
 return (n);
 }
