4989240074fcb926ef216acf485d4fbaf66707e3
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-10 16:33:02 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,11 @@
 void _puts_recursion(char *s)
 {
 int i = 0;
-while (s[i] != '\0')
-{
 _putchar(s[i]);
 i++;
-}
-_putchar('\0');
+_puts_reacursion(char *s);
+if(s[i] == '\0')
+{
 _putchar('\n');
+}
 }
