e84ea546a013d85c2ba6adcb37ed7a1145b2053a
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-10 18:26:06 -0700

    pow_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: the number
+ * Return: sqaure root of n
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+return (_sqrt_recursion_2(n,0));
+}
+/**
+ * _sqrt_recursion_2 - natural square root of a number
+ * @n: the number
+ * @i: the square
+ * Return: sqaure root of n
+ */
+int _sqrt_recursion_2(int n, int i)
+{
+if ((n / i) == i)
+{
+return (i);
+}
+else
+{
+return (-1);
+}
+return _sqrt_recursion_2(n, i + 1);
+}
