4132fa19031539c0b7ff743d546455e11e88c93f
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-13 17:41:36 -0700

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -44,9 +44,9 @@ return (n);
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: 0 (Success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
