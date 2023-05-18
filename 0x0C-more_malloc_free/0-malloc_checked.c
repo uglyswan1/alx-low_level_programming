9919e1af9daf63266aea5413a0a1ab251744237c
uglyswan1 <knza.hrsn.14@gmail.com>
2023-05-18 16:30:54 -0700

    0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * malloc_checked -  allocates memory using malloc
@@ -16,8 +17,5 @@ if (all == NULL)
 {
 exit(98);
 }
-else
-{
 return (all);
-}
 }
